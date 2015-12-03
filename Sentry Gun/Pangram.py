20c5b8e834b11f5d0c63c37b22b5a6e16c59e916
sofiannasof <s0fianna.menesidou@gmail.com>
2015-12-04 01:26:37 +0200

    Pangram

---
+++ Sentry Gun/Pangram.py
@@ -0,0 +1,14 @@
+def check_pangram(text):
+    alphabet = ['W','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    for letter in alphabet:
+        if not letter.lower() in text.lower():
+            return False
+    return True
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert check_pangram("The quick brown fox jumps over the lazy dog."), "brown fox"
+    assert not check_pangram("ABCDEF"), "ABC"
+    assert check_pangram("Bored? Craving a pub quiz fix? Why, just come to the Royal Oak!"), "Bored?"
+
+    print("All done? Earn rewards by using the 'Check' button!")
